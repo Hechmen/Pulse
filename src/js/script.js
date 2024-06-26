3d2f0c2431ba10dae23127c67185b47e12866523
Hechmen <bashikov2014@gmail.com>
2024-06-26 22:27:27 +0100

    Removed slick and made carousel adaptive

--- src/js/script.js
+++ src/js/script.js
@@ -1,34 +1,11 @@
-// $(document).ready(function(){
-//     $('.carousel__inner').slick({
-//         dots: true
-//     });
-// });
-
 const carousel = document.querySelector(".carousel__inner"),
 firstImg = carousel.querySelectorAll("img")[0];
 arrowIcons = document.querySelectorAll(".container i");
-
-// if (!carouselInner || !firstImg || !arrowIcons) {
-//     console.error("One or more elements are not found in the DOM.");
-//     return;
-// }
 
 let firstImgWidth = firstImg.clientWidth + 30; //getting first img width & adding doubled 15 margin value
 
 arrowIcons.forEach(icon => {
     icon.addEventListener("click", () => {
-        // if the left icon is clicked, reduce width value from the carousel scroll left else add to it
-        // if(icon.id == "left") {
-        //     carousel.scrollRight += firstImgWidth;
-        // } else {
-        //     carousel.scrollLeft += firstImgWidth;
-        // }
-
-        // if(icon.id == "right") {
-        //     carousel.scrollLeft -= firstImgWidth;
-        // } else {
-        //     carousel.scrollLeft += firstImgWidth;
-        // }
 
         if(icon.id == "right"){
             carousel.scrollLeft += firstImgWidth;
