c00322a258b6f5da75bca5c10f93fa54c1f837dc
Hechmen <Bashikov2014@gmail.com>
2024-08-20 18:05:51 +0500

    Added php mailer

--- src/js/script.js
+++ src/js/script.js
@@ -61,7 +61,7 @@ $('.button_mini').each(function(i) {
     });
 });
 
-//Validation
+//Form validation
 function validateForms(form) {
     $(form).validate({
         rules: {
@@ -89,3 +89,23 @@ validateForms('#order form');
 
 //Phone format
 $('input[name=phone]').mask("+7 (999) 999-9999")
+
+$('form').submit(function() {
+    e.preventDefault();
+
+    if (!$(this).valid()) {
+        return;
+    }
+
+    $.ajax({
+        type: "POST",
+        url: "mailer/smart.php",
+        data: $(this).serialize()
+    }).done(function() {
+        $(this).find("input").val("");
+
+
+        $('form').trigger('reset'); 
+    });
+    return false;
+});
